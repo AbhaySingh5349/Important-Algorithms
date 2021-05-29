1f953ba81941254526aab089a7eda60645ee87ae
Abhay Singh <abhay5349singh@gmail.com>
2021-05-30 01:13:34 +0530

    Add files via upload

---
+++ Topological Sort.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+string itos(T i){
+	stringstream s;
+	s<<i;
+	return s.str();
+}
+
+#define N 100001
+
+vector<int> graph[N];
+bool visited[N];
+
+void topoSort(int src, stack<int> &st){
+	visited[src]=true;
+	for(int i=0;i<graph[src].size();i++){
+		int child=graph[src][i];
+		if(visited[child]==false){
+			topoSort(child,st);
+		}
+	}
+	st.push(src);
+}
+
+int main()
+{	
+	int vertices, edges;
+	cin>>vertices>>edges;
+	int i,j;
+	for(i=0;i<edges;i++){
+		int u,v;
+		cin>>u>>v;
+		graph[u].push_back(v);
+	}
+	
+	stack<int> st;	
+	memset(visited,false,N);
+	for(i=1;i<=vertices;i++){
+		if(visited[i]==false){
+			topoSort(i,st);
+		}
+	}
+	
+	while(st.size()>0){
+		int x=st.top();
+		st.pop();
+		cout<<x<<" ";
+	}
+	return 0;
+}
