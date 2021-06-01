c164e553182ebd2e073cbf7e03858ad48a90eb8b
Abhay Singh <abhay5349singh@gmail.com>
2021-06-02 01:23:47 +0530

    Update 2-D prefix sum.cpp

--- 2-D prefix sum.cpp
+++ 2-D prefix sum.cpp
@@ -11,16 +11,16 @@ int main()
     	int i,j;
         
     	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-		    pre[i][j]=mat[i][j];
-		    if(i>0) pre[i][j]+=pre[i-1][j];
-		    if(j>0) pre[i][j]+=pre[i][j-1];
-		    if(i>0 && j>0) pre[i][j]-=pre[i-1][j-1];
+	   for(j=0;j<m;j++){
+		pre[i][j]=mat[i][j];
+                if(i>0) pre[i][j]+=pre[i-1][j];
+		if(j>0) pre[i][j]+=pre[i][j-1];
+		if(i>0 && j>0) pre[i][j]-=pre[i-1][j-1];
 
-		    cout<<pre[i][j]<<" ";
-		}
-		cout<<"\n";
-		}
+		cout<<pre[i][j]<<" ";
+	    }
+	    cout<<"\n";
+        }
 	cout<<(pre[x2][y2]-pre[x2][y1-1]-pre[x1-1][y2]+pre[x1-1][y1-1]);
 	return 0;
 }
