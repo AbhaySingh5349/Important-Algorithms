ad0d7338bd8b25f93e6b83751839f357416d1766
Abhay Singh <abhay5349singh@gmail.com>
2021-06-02 01:21:53 +0530

    Add files via upload

---
+++ 2-D prefix sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+string itos(T i){
+	stringstream s;
+	s<<i;
+	return s.str();
+}
+    
+int main()
+{
+	int mat[n][m];
+	
+	int pre[n][m];
+	memset(pre,0,sizeof(pre));
+        
+    int i,j;
+        
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            pre[i][j]=mat[i][j];
+            if(i>0) pre[i][j]+=pre[i-1][j];
+            if(j>0) pre[i][j]+=pre[i][j-1];
+            if(i>0 && j>0) pre[i][j]-=pre[i-1][j-1];
+                
+            cout<<pre[i][j]<<" ";
+        }
+        cout<<"\n";
+	}
+	cout<<(pre[x2][y2]-pre[x2][y1-1]-pre[x1-1][y2]+pre[x1-1][y1-1]);
+	return 0;
+}
