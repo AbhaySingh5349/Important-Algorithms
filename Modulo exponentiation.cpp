f6e1a0fd604cf25010940eb1c9f86346015b22ee
Abhay Singh <abhay5349singh@gmail.com>
2021-06-10 02:35:05 +0530

    Add files via upload

---
+++ Modulo exponentiation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+string itos(T i){
+	stringstream s;
+	s<<i;
+	return s.str();
+}
+
+long long int power(long long int a, long long int b, int mod){
+    long long int ans=1;
+    while(b){
+        if(b%2==0){
+            a = (((a+mod)%mod)*((a+mod)%mod))%mod;
+            b/=2;
+        }else{
+            ans = (((ans+mod)%mod)*((a+mod)%mod))%mod;
+            b--;
+        }
+    }
+    return ans;
+}
+    
+int main()
+{
+	int x,n,mod;
+	if(x==0) return 0;
+    cout<<power(x,n,d);
+    
+	return 0;
+}
+
