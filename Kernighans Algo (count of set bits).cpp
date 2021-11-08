7950005081e9a2943eae716b993aaf22635b4c62
Abhay Singh <abhay5349singh@gmail.com>
2021-11-08 17:02:16 +0530

    Update Kernighans Algo (count of set bits).cpp

--- Kernighans Algo (count of set bits).cpp
+++ Kernighans Algo (count of set bits).cpp
@@ -1,9 +1,11 @@
+Question Link: https://leetcode.com/problems/number-of-1-bits/
+
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int c=0;
         while(n>0){
-            int rmsb=n&(-n);
+            int rmsb=n&(-n); // rightmost set bit
             n-=rmsb;
             c++;
         }
