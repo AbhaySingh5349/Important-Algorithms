ca770e261b4c3659df158163959356c99f2a9008
Abhay Singh <abhay5349singh@gmail.com>
2021-11-10 00:46:49 +0530

    Update Kadanes Algo (maximum sum subarray).cpp

--- Kadanes Algo (maximum sum subarray).cpp
+++ Kadanes Algo (maximum sum subarray).cpp
@@ -1,6 +1,23 @@
 #include <vector>
 using namespace std;
 
+// Approach 1:
+
+int kadanesAlgorithm(vector<int> a) {
+  int n=a.size();
+	int dp[n];
+        int maxsum=a[0];
+        dp[0]=a[0];
+        
+        for(int i=1;i<n;i++){
+            dp[i]=a[i] + (dp[i-1]>0 ? dp[i-1]:0);
+            maxsum=max(maxsum,dp[i]);
+        }
+  return maxsum;
+}
+
+// Approach 2:
+
 int kadanesAlgorithm(vector<int> a) {
   int n=a.size();
 	int maxsum=INT_MIN, s=0;
