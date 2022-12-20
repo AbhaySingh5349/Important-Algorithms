0458982fcbd739a56ee5a05033546a4a797f40bd
Abhay Singh <abhay5349singh@gmail.com>
2022-12-20 17:44:27 +0530

    Update Topological Sort.cpp

--- Topological Sort.cpp
+++ Topological Sort.cpp
@@ -5,49 +5,6 @@ using namespace std;
 // jobs:[1,2,3,4], deps:[[1,2],[1,3],[3,2],[4,2],[4,3]]
 // o/p: [1,4,3,2] or [4,1,3,2]
 
-// Approach 1: using DFS
-
-void toposort(vector<int> graph[], stack<int> &st,int src, bool visited[]){
-	visited[src]=true;
-	for(int i=0;i<graph[src].size();i++){
-		int child=graph[src][i];
-		if(visited[child]==false){
-			toposort(graph,st,child,visited);
-		}
-	}
-	st.push(src);
-}
-
-vector<int> topologicalSort(vector<int> jobs, vector<vector<int>> deps) {
-  	int n=jobs.size();
-	int maxe=*max_element(jobs.begin(),jobs.end());
-	vector<int> graph[maxe+1];
-	for(int i=0;i<deps.size();i++){
-		int u=deps[i][0], v=deps[i][1];
-		graph[u].push_back(v);
-	}
-	stack<int> st;
-	
-	bool visited[maxe+1];
-	memset(visited,false,sizeof(visited));
-	
-	for(int i=0;i<n;i++){
-		if(visited[jobs[i]]==false){
-			toposort(graph,st,jobs[i],visited);
-		}
-	} 
-	vector<int> v;
-	if(st.size()!=n) return v;
-	
-	while(st.size()>0){
-		v.push_back(st.top());
-		st.pop();
-	}
-  return v;
-}
-
-// Approach 2: using BFS 
-
 #include <vector>
 using namespace std;
 
