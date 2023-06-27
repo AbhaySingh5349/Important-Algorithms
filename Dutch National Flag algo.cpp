305a83ec595bb99d766dc8ee2fa9a83f151fb58f
Abhay Singh <abhay5349singh@gmail.com>
2023-06-27 15:32:20 +0530

    Update Dutch National Flag algo.cpp

--- Dutch National Flag algo.cpp
+++ Dutch National Flag algo.cpp
@@ -1,6 +1,6 @@
 Question : sort array of 0,1
 
-// Approach : 
+// Approach : [0,i] belongs to 0's , [i+1,j-1] belongs to 1's and [j,n-1] is undefined region
 class Solution {
 public:
     void moveZeroes(vector<int>& a) {
