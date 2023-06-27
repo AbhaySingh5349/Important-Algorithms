50a87152ef8abbd07e591072484263bdb8fea703
Abhay Singh <abhay5349singh@gmail.com>
2023-06-27 15:54:50 +0530

    Update Dutch National Flag algo.cpp

--- Dutch National Flag algo.cpp
+++ Dutch National Flag algo.cpp
@@ -1,6 +1,6 @@
 Question : sort array of 0,1
 
-// Approach : [0,i] belongs to 0's , [i+1,j-1] belongs to 1's and [j,n-1] is undefined region
+// Approach : [0,i] => 0's , [i+1,j-1] => 1's and [j,n-1] => undefined
 class Solution {
 public:
     void moveZeroes(vector<int>& a) {
@@ -13,6 +13,30 @@ public:
                 swap(a[i],a[j]);
             }
             j++;
+        }
+    }
+};
+
+Question : sort array of 0,1,2
+
+ // Approach : [0,i] => 0's , [i+1,j-1] => 1's, [j,k] => undefined, [k+1,n-1]: 2's
+class Solution {
+public:
+    void sortColors(vector<int>& a) {
+        int n=a.size();
+        
+        int i=-1, j=0, k=n-1;
+        while(j<=k){
+            if(a[j] == 0){
+                i++;
+                swap(a[i],a[j]);
+                j++;
+            }else if(a[j] == 2){
+                swap(a[j],a[k]);
+                k--;
+            }else{
+                j++;
+            }
         }
     }
 };
